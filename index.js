f9e796f3c0a8ee511fce9887b0695935e8546b15
Daniela Farias <farias.danicris@gmail.com>
2021-11-07 19:22:57 -0300

    Criação da página de detalhes, e array de pokemons

--- index.js
+++ index.js
@@ -1,27 +1,75 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var pokemon = [
+  {
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    nome: "Bulbasaur",
+    numero: "001",
+    tipo: "Grass",
+  },
+  {
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+    nome: "Ivysaur",
+    numero: "002",
+    tipo: "Grass",
+  },
+  {
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+    nome: "Venusaur",
+    numero: "003",
+    tipo: "Grass",
+  },
+];
 
 app.get("/", (req, res) => {
-  const devList = ["Backend", "Frontend", "Fullstack"];
-  const analyticsList = ["Engenharia de dados", "Ciência de dados"];
-  res.render("index", { titulo: "Pokédex | Node.js", devList: devList, analyticsList: analyticsList });
+  res.render("index", { titulo: "Pokédex | Node.js", pokemons: pokemon });
 });
 
 app.get("/register", (req, res) => {
   res.render("register", { titulo: "Pokédex | Registro" });
 });
 
-app.post("/pokemon", (req, res) => {
-  const { nome, numero, descricao, tipo, altura, peso, habilidade, categoria } = req.body;
-  res.send({ nome: nome, numero: numero, descricao: descricao, tipo: tipo, altura: altura, peso: peso, habilidade: habilidade, categoria: categoria });
+app.get("/details/:numero", (req, res) => {
+  res.render("details", { titulo: "Pokédex | Detalhes", pokemons: pokemon, numero: req.params.numero });
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded());
+app.post("/pokemon", (req, res) => {
+  const {
+    imagem,
+    nome,
+    numero,
+    descricao,
+    tipo,
+    altura,
+    peso,
+    habilidade,
+    categoria,
+  } = req.body;
+  const object = {
+    imagem: imagem,
+    nome: nome,
+    numero: numero,
+    descricao: descricao,
+    tipo: tipo,
+    altura: altura,
+    peso: peso,
+    habilidade: habilidade,
+    categoria: categoria,
+  };
 
+  pokemon.push(object);
+  res.redirect("/");
+});
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
